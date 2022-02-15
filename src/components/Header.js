211d2aec10dfae9b65618dc7b85f4a16d01a0fc6
Jamie Bunch <JamieBunch@ymail.com>
2022-02-15 17:03:05 -0500

    updated code due to complier issue

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import {UserContext} from '../App';
+
+function Header(){
+    const {user, setUser} = useContext(UserContext)
+    return (
+        <header>
+            <nav>
+                {user
+                ? <button onClick={() => setUser(null)}>Logout</button>
+                : <button onClick={() => setUser('Jamie')}>Login</button>
+                }
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
